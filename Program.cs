f16f4c103f521cb4436dad59433e471d4671fd04
Pavel Mareček <marecekpavel28704@gmail.com>
2021-11-22 14:10:14 +0100

    something

--- Program.cs
+++ Program.cs
@@ -20,7 +20,22 @@ namespace MazeGenerator
                     maze[i, j] = "x";
                     Console.Write(string.Format("{0} ", maze[i, j]));
                 }
+<<<<<<< Updated upstream
                 Console.Write(Environment.NewLine + Environment.NewLine);
+=======
+            }
+
+            Console.WriteLine("Ola");
+
+            for (int i = 0;i < maze.GetLength(0); i++)
+            {
+                for (int j = 0; j < maze.GetLength(1); j++)
+                {
+                    var item = maze[i, j];
+                    Console.Write(item + " ");
+                }
+                Console.Write(Environment.NewLine);
+>>>>>>> Stashed changes
             }
         }
     }
